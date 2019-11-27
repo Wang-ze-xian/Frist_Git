e5e5de236c9722e0b75276719fbf94a6d0c0372d
王泽贤 <742975299@qq.com>
2019-11-27 17:36:47 +0800

    git commit again

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("LPL NB!");
         System.out.println("FPX IG NB!");
+        System.out.println("RNG NB!");
     }
 }
